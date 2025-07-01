efb11af38322b47456fb8e7282ba18815f547ffe
Akash Singh <akash.singh13503@gmail.com>
2025-07-01 16:00:25 +0530

    backend server ready to deploy

---
+++ server.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose")
+const Document = require("./Document")
+require('dotenv').config();
+
+const mongo_string = process.env.MONGO_URI;
+
+mongoose.connect(mongo_string)
+
+const http = require("http");
+const express = require("express");
+const app = express();
+const server = http.createServer(app);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+const defaultValue = ""
+
+io.on("connection", socket => {
+  socket.on("get-document", async documentId => {
+    const document = await findOrCreateDocument(documentId)
+    socket.join(documentId)
+    socket.emit("load-document", document.data)
+ 
+    socket.on("send-changes", delta => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta)
+    })
+
+    socket.on("save-document", async data => {
+      await Document.findByIdAndUpdate(documentId, { data })
+    })
+  })
+})
+
+
+async function findOrCreateDocument(id) {
+  if (id == null) return
+
+  const document = await Document.findById(id)
+  if (document) return document
+  return await Document.create({ _id: id, data: defaultValue })
+}
